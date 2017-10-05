589fe91bb7c70456f9f2e1bdd5dc75cc1b75049f
Barry O'Rourke <barry@orodor.org.uk>
2017-10-05 20:50:38 +0100

    revert version, update owner details and adhere to CHANGELOG standards

---
+++ CHANGELOG.md
@@ -0,0 +1,18 @@
+# Change Log
+This project adheres to [Semantic Versioning](http://semver.org/).
+
+This CHANGELOG follows the format listed [here](https://github.com/sensu-plugins/community/blob/master/HOW_WE_CHANGELOG.md)
+
+## [Unreleased]
+### Added
+- Added ability to submit checks for a jit host (@PhilipHarries)
+
+### Changed
+- Update Changelog to comply with standards (@barryorourke)
+- Update Ownership in setup.py (@barryorourke)
+
+## [0.2.0] 2014-01-06
+- Add support for Python3 (@zsprackett)
+
+## [0.1.0] 2014-01-06
+- Initial release (@zsprackett)
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name='sensu_plugin',
-    version='0.3.0',
-    author='S. Zachariah Sprackett',
-    author_email='zac@sprackett.com',
+    version='0.2.0',
+    author='Sensu-Plugins and contributors',
+    author_email='sensu-users@googlegroups.com',
     packages=['sensu_plugin', 'sensu_plugin.test'],
     scripts=[],
-    url='http://github.com/sensu/python_sensu_plugin/',
+    url='https://github.com/sensu-plugins/sensu-plugin-python',
     license='LICENSE.txt',
     description='A framework for writing Python sensu plugins.',
     long_description="""
