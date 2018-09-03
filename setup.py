26a2876a08ea81b8f57dc32956846c54faf5357f
Barry O'Rourke <barry@orodor.org.uk>
2018-09-03 17:22:49 +0100

    update setup.py metadata

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='sensu_plugin',
     version='0.4.7',
-    author='Sensu-Plugins and contributors',
+    author='Sensu-Plugins and Contributors',
     author_email='sensu-users@googlegroups.com',
     packages=['sensu_plugin', 'sensu_plugin.test'],
     scripts=[],
@@ -17,7 +17,9 @@ setup(
         'requests'
     ],
     tests_require=[
-        'pep8',
-        'pylint'
+        'pycodestyle',
+        'pylint',
+        'coverage',
+        'nose'
     ],
 )
