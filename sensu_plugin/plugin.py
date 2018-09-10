727c353415f3dc968088123630aada1a9addc181
Barry O'Rourke <barry@orodor.org.uk>
2018-09-10 10:23:12 +0100

    tidy up and add docstrings to plugin.py

--- sensu_plugin/plugin.py
+++ sensu_plugin/plugin.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# Copyright (C) 2014 - S. Zachariah Sprackett <zac@sprackett.com>
-#
-# Released under the same terms as Sensu (the MIT license); see LICENSE
-# for details.
 
 from __future__ import print_function
-import atexit
-import sys
+
 import argparse
+import atexit
 import os
+import sys
 import traceback
+
 from collections import namedtuple
+
 from sensu_plugin.exithook import ExitHook
 
+# create a namedtuple of all valid exit codes
 ExitCode = namedtuple('ExitCode', ['OK', 'WARNING', 'CRITICAL', 'UNKNOWN'])
 
 
 class SensuPlugin(object):
+    '''
+    Base class used by both checks and metrics plugins.
+    '''
     def __init__(self):
+
+        # barryorourke: revist this later
         self.plugin_info = {
             'check_name': None,
             'message': None,
@@ -28,12 +32,15 @@ class SensuPlugin(object):
         self._hook = ExitHook()
         self._hook.hook()
 
+        # create a method for each of the exit codes
+        # and register as exiy functions
         self.exit_code = ExitCode(0, 1, 2, 3)
         for field in self.exit_code._fields:
             self.__make_dynamic(field)
 
         atexit.register(self.__exitfunction)
 
+        # Prepare command line arguments
         self.parser = argparse.ArgumentParser()
         if hasattr(self, 'setup'):
             self.setup()
@@ -45,7 +52,9 @@ class SensuPlugin(object):
         print("SensuPlugin: %s" % ' '.join(str(a) for a in args))
 
     def __make_dynamic(self, method):
-
+        '''
+        Create a method for each of the exit codes.
+        '''
         def dynamic(*args):
             self.plugin_info['status'] = method
             if not args:
@@ -59,9 +68,16 @@ class SensuPlugin(object):
         setattr(self, dynamic.__name__, dynamic)
 
     def run(self):
+        '''
+        Method should be overwritten by inherited classes.
+        '''
         self.warning("Not implemented! You should override SensuPlugin.run()")
 
     def __exitfunction(self):
+        '''
+        Method called by exit hook, ensures that both an exit code and
+        output is supplied, also catches errors.
+        '''
         if self._hook.exit_code is None and self._hook.exception is None:
             print("Check did not exit! You should call an exit code method.")
             sys.stdout.flush()
